f98068106458176557a1484df7fafb5348c67d10
derlee <derlee@gmail.com>
2025-04-21 17:08:02 +0800

    Update Celery worker command to specify gevent pool for improved performance

--- entrypoint.worker.sh
+++ entrypoint.worker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec celery -A app.celery.worker.celery worker --loglevel=info --autoscale=${ENV_CELERY_CONCURRENCY:-4},2 --max-tasks-per-child=${ENV_CELERY_MAX_TASKS_PER_CHILD:-100}
+exec celery -A app.celery.worker.celery worker --pool gevent --loglevel=info --autoscale=${ENV_CELERY_CONCURRENCY:-4},2 --max-tasks-per-child=${ENV_CELERY_MAX_TASKS_PER_CHILD:-100}
